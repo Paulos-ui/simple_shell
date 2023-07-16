f4465063dfc442c63b2f847930a3cd60b2be963b
Paul <tunmisejayeoba@gmail.com>
2023-07-16 15:42:20 -0700

    simple_shell

---
+++ exit_shell.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * exit_shell - manages the execution of the shell exit
+ * @line_tkn: input read from standard input
+ * author: Jayeoba tunmise paul
+ * @command: tokenized command
+ * Return: NULL
+ */
+void exit_shell(char **command, char *line_tkn)
+{
+	free(line_tkn);
+	free_buffer_function(command);
+	exit(0);
+}
